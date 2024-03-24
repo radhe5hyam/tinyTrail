e17750a210b44aec211c847bc76dd0cccebbdc98
Tard <rst.choudhary10@gmail.com>
2024-03-24 09:41:00 +0530

    single prisma connection

--- src/app/api/shorten/route.ts
+++ src/app/api/shorten/route.ts
@@ -1,8 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
+import prisma from "../../../lib/db";
 import { nanoid } from "nanoid";
-
-const prisma = new PrismaClient();
 
 export async function POST(req: NextRequest) {
   const { original } = await req.json();
