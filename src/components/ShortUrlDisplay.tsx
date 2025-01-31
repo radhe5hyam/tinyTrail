61a3dc753b6db361a262c66b7e501b238baffab6
Radheshyam Tard <rst.choudhary10@gmail.com>
2025-02-01 04:51:00 +0530

    v2

--- src/components/ShortUrlDisplay.tsx
+++ src/components/ShortUrlDisplay.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 import { MdCopyAll, MdInsertLink, MdOpenInNew } from "react-icons/md";
 import { BsMagic } from "react-icons/bs";
+import { Input } from "@/components/ui/input";
 
 interface ShortenedUrlDisplayProps {
   longUrl: string;
@@ -18,34 +19,46 @@ const ShortUrlDisplay: React.FC<ShortenedUrlDisplayProps> = ({
 }) => {
   return (
     <>
-      <div className="mt-4">
-        <p className="flex items-center">
+      <div className="mb-4 w-full">
+        <div className="flex items-center mb-2">
           <MdInsertLink className="mr-2" />
-          Original URL:
-        </p>
-        <p>{longUrl}</p>
-        <p className="mt-4 flex items-center">
-          <BsMagic className="mr-2" />
-          Tiny Trail URL:
-        </p>
-        <p>{shortUrl}</p>
+          <span className="font-bold">Original URL:</span>
+        </div>
+        <Input
+          type="text"
+          readOnly
+          className="w-full my-4 rounded-full h-12"
+          value={longUrl}
+        />
       </div>
-      <div className="flex space-x-4">
+      <div className="mb-4 w-full">
+        <div className="flex items-center mb-2">
+          <BsMagic className="mr-2" />
+          <span className="font-bold">Tiny Trail URL:</span>
+        </div>
+        <Input
+          type="text"
+          readOnly
+          className="w-full my-4 rounded-full h-12"
+          value={shortUrl}
+        />
+      </div>
+      <div className="flex space-x-4 mb-4 w-full">
         <Button
-          className="w-full max-w-md my-4 bg-blue-500 text-white px-4 py-2 rounded-lg"
+          className="w-full bg-blue-500 text-white px-4 py-2 rounded-lg"
           onClick={() => window.open(shortUrl, "_blank")}
         >
-          <MdOpenInNew /> Visit URL
+          <MdOpenInNew className="mr-2" /> Visit URL
         </Button>
         <Button
-          className="w-full max-w-md my-4 bg-blue-500 text-white px-4 py-2 rounded-lg"
+          className="w-full bg-blue-500 text-white px-4 py-2 rounded-lg"
           onClick={handleCopy}
         >
-          <MdCopyAll /> Copy
+          <MdCopyAll className="mr-2" /> Copy
         </Button>
       </div>
       <Button
-        className="w-full max-w-md my-4 bg-gray-500 text-white px-4 py-2 rounded-lg"
+        className="w-full bg-gray-500 text-white px-4 py-2 rounded-lg"
         onClick={handleReset}
       >
         Shorten Another URL
